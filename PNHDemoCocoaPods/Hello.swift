ba39018360d042ce096300cb7420fe080c2df1f6
hanh-pham-n <hanhphamn@gmail.com>
2017-11-23 14:43:50 +0700

    init

---
+++ PNHDemoCocoaPods/Hello.swift
@@ -0,0 +1,15 @@
+//
+//  Hello.swift
+//  PNHDemoCocoaPods
+//
+//  Created by Pham Ngoc Hanh on 11/23/17.
+//  Copyright © 2017 Pham Ngoc Hanh. All rights reserved.
+//
+
+import Foundation
+
+public class HelloCocoaPod {
+    class func hello() -> String {
+        return "a"
+    }
+}
---
+++ PNHDemoCocoaPods/PNHDemoCocoaPods.h
@@ -0,0 +1,19 @@
+//
+//  PNHDemoCocoaPods.h
+//  PNHDemoCocoaPods
+//
+//  Created by Pham Ngoc Hanh on 11/23/17.
+//  Copyright © 2017 Pham Ngoc Hanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PNHDemoCocoaPods.
+FOUNDATION_EXPORT double PNHDemoCocoaPodsVersionNumber;
+
+//! Project version string for PNHDemoCocoaPods.
+FOUNDATION_EXPORT const unsigned char PNHDemoCocoaPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PNHDemoCocoaPods/PublicHeader.h>
+
+
